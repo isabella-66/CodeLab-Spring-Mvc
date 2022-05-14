be09768ce6f4c38c673cf1f918a755353a28c5f4
isabella-66 <amasprog@gmail.com>
2022-05-14 16:32:19 -0300

    commit hello

--- src/main/java/br/com/ifsp/regescweb/controllers/HelloController.java
+++ src/main/java/br/com/ifsp/regescweb/controllers/HelloController.java
@@ -1,14 +1,31 @@
 package br.com.ifsp.regescweb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
+    public ModelAndView hello(){
+        ModelAndView mv = new ModelAndView("hello"); //nome do arquivo para renderização
+        mv.addObject("nome", "Maria");
+        return mv; //renderização arquivo em templates/hello.html;
+    }
+
+    @GetMapping("/hello-model")
+    public String hello(Model model){
+        model.addAttribute("nome", "Maria");
+        return "hello";
+    }
+
+    @GetMapping("/hello-servlet")
     public String hello(HttpServletRequest request){
-        request.setAttribute(name:"nome", o:"Ana Maria");
-        return "hello"; //renderização arquivo em templates/hello.html
+        request.setAttribute("nome", "Zezinho");
+        return "hello"; //renderização arquivo em templates/hello.html;
     }
 }
